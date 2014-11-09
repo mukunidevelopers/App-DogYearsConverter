f7e8d6fe90bda918bbde4a0db23329d980cdfed0
Arthur Labisi Siloka <Silokas_Mac@Arthurs-MacBook-Pro.local>
2014-11-09 21:26:51 +0800

    first commit

--- DogYearConverter/ViewController.swift
+++ DogYearConverter/ViewController.swift
@@ -9,6 +9,8 @@
 import UIKit
 
 class ViewController: UIViewController {
+    @IBOutlet weak var dogYearLabel: UILabel!
+    @IBOutlet weak var enterHumanYearsTextField: UITextField!
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -18,6 +20,19 @@ class ViewController: UIViewController {
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
+    }
+    
+    @IBAction func convertDogYearButtonPressed(sender: UIButton) {
+        
+        let stringFromTextField = enterHumanYearsTextField.text
+        let optionalIntFromTextField = stringFromTextField.toInt()
+        let intFromOptional = optionalIntFromTextField!
+        
+        dogYearLabel.text = "\(intFromOptional * 7)" + " Human Years"
+        
+        dogYearLabel.hidden = false
+        
+        enterHumanYearsTextField.resignFirstResponder()
     }
 
 
